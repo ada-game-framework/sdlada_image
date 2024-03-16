82e99715e13893a37fb4703aa33203c98a4ec01e
Luke A. Guest <laguest@archeia.com>
2024-03-16 19:32:11 +0000

    Add linker options.

---
+++ src/link/macosx/sdl-image_linker.ads
@@ -0,0 +1,10 @@
+--------------------------------------------------------------------------------------------------------------------
+--  This source code is subject to the Zlib license, see the LICENCE file in the root of this directory.
+--------------------------------------------------------------------------------------------------------------------
+--  SDL.Mixer.Linker
+--------------------------------------------------------------------------------------------------------------------
+package SDL.Image_Linker is
+   pragma Pure;
+
+   Image_Linker_Options : constant String := "-framework SDL2_image";
+end SDL.Image_Linker;
