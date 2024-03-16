00d0b18346e125e3c81c911a9e447d52ca33c691
Luke A. Guest <laguest@archeia.com>
2024-03-16 11:49:41 +0000

    Add missing formats.

--- src/sdl-images.ads
+++ src/sdl-images.ads
@@ -7,8 +7,11 @@
 --------------------------------------------------------------------------------------------------------------------
 with Interfaces.C;
 
+with SDL.Image_Linker;
+
 package SDL.Images is
    pragma Pure;
+   pragma Linker_Options (SDL.Image_Linker.Image_Linker_Options);
 
    package C renames Interfaces.C;
 
@@ -20,17 +23,22 @@ package SDL.Images is
    Enable_PNG        : constant Init_Image_Flags := 16#0000_0002#;
    Enable_TIFF       : constant Init_Image_Flags := 16#0000_0004#;
    Enable_WEBP       : constant Init_Image_Flags := 16#0000_0008#;
-   Enable_Everything : constant Init_Image_Flags := Enable_JPG or Enable_PNG or Enable_TIFF or Enable_WEBP;
+   Enable_JXL        : constant Init_Image_Flags := 16#0000_0010#;
+   Enable_AVIF       : constant Init_Image_Flags := 16#0000_0020#;
+   Enable_Everything : constant Init_Image_Flags :=
+     Enable_JPG or Enable_PNG or Enable_TIFF or Enable_WEBP or Enable_JXL;
 
-   type Formats is (Targa, Cursor, Icon, BMP, GIF, JPG, LBM, PCX, PNG, PNM, TIFF, XCF, XPM, XV, WEBP);
+   type Formats is (AVIF, BMP, Cursor, Icon, GIF, JPG, JXL, LBM, PCX, PNG,
+     PNM, SVG, QOI, Targa, TIFF, XCF, XPM, XV, WEBP);
 
-   overriding
    function Initialise (Flags : in Init_Image_Flags := Enable_Everything) return Boolean;
 
-   procedure Finalise with
+   procedure Quit with
      Import        => True,
      Convention    => C,
      External_Name => "IMG_Quit";
+
+   procedure Finalise renames Quit;
 private
    subtype Format_String_Names is C.char_array (1 .. 5);
    type Format_String_Arrays is array (Formats) of Format_String_Names;
